afefedc8d7166426c293dc2744f4028052a44520
Ayush <nairayush45@gmail.com>
2026-02-19 01:55:17 +0530

    commit

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel.js"); // Import User model
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // User Registration
